931aaf866b68bcebe01974adf7bfa2404567e207
WM-BD000031 <wm-bd000031@Ella-iMac.local>
2024-03-04 18:26:26 +0900

    stop(March 4): video 4.4

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	"crypto/sha256"
-	"fmt"
-)
+import "github.com/minjoo0729/pencoin/blockchain"
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
+
 
 func main() {
-	genesisBlock := block{"Genesis Block", "", ""}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	hexHash := fmt.Sprintf("%x", hash)
-	genesisBlock.hash = hexHash
-	fmt.Println(genesisBlock)
+	chain := blockchain.GetBlockchain()
 }
