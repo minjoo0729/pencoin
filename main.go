1861dd365619eca6c70cc6cca1ec07a7c13c27dc
WM-BD000031 <wm-bd000031@Ella-iMac.local>
2024-03-05 17:32:10 +0900

    feat(JSON): setup and hard way

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import explorer "github.com/minjoo0729/pencoin/explorer/templates"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/minjoo0729/pencoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s\n", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
