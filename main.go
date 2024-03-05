f49340f8585378546cac3967a1dc346366d24846
WM-BD000031 <wm-bd000031@Ella-iMac.local>
2024-03-05 10:38:56 +0900

    feat(web): home page and response writer

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 
 const port string = ":4000"
 
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from home!")
+}
+
 func main() {
+	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
