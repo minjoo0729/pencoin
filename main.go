9b906249eb29f9d05d1bd4ea6c8afcc080e7280f
WM-BD000031 <wm-bd000031@Ella-iMac.local>
2024-03-05 11:32:31 +0900

    feat(web): rendering blocks

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("./templates/home.html"))
+	tmpl := template.Must(template.ParseFiles("./templates/pages/home.gohtml"))
 	data := homeData{"Minjoo", blockchain.GetBlockchain().AllBlocks()}
 	tmpl.Execute(rw, data)
 }
