8d92e64239707a65f55e550965727e50ace27cba
WM-BD000031 <wm-bd000031@Ella-iMac.local>
2024-03-06 17:40:01 +0900

    feat(JSON): Gorilla Mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/minjoo0729/pencoin
 
 go 1.22.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	explorer "github.com/minjoo0729/pencoin/explorer/templates"
 	"github.com/minjoo0729/pencoin/rest"
 )
 
 
 func main() {
-	go explorer.Start(3000)
 	rest.Start(4000)
 }
